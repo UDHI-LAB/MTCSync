42cc51d6b4aa70c7d0aabb532972251f0aba595c
yugu-0202 <applerin1119@gmail.com>
2023-12-22 02:15:05 +0900

    ファイルのロードとタイムコードの抽出処理

--- mtcsync/mtcsync.py
+++ mtcsync/mtcsync.py
@@ -3,22 +3,45 @@ import mido
 from timecode import Timecode
 import questionary
 import os
+import json
+import re
 import mpvex
 import mtc
 
 
 midi_port = questionary.select(
-    "select MIDI port",
+    "select MIDI in port",
     choices=mido.get_input_names()
 ).ask()
 
 m3u_path = questionary.path(
     "set m3u path",
-    file_filter=lambda path: os.path.splitext(path)[1] == ".m3u"
+    #file_filter=lambda path: os.path.splitext(path)[1] == ".m3u"
 ).ask()
 
+timeline_path = questionary.path(
+    "set timeline path",
+    #file_filter=lambda path: os.path.splitext(path)[1] == ".json"
+).ask()
+
+with open(timeline_path, "r", encoding="utf-8") as f:
+    text = f.read()
+re_text = re.sub(r'/\*[\s\S]*?\*/|//.*', '', text)
+timeline_json = json.loads(re_text)
+
+config_id = questionary.select(
+    "select machine id",
+    choices=[questionary.Choice(title=f"{c['id']} {c['name']}",value=f"{c['id']}") for c in timeline_json["config"]]
+).ask()
+
+print(config_id)
+
 port = mido.open_input(midi_port)
-timecodes: List[str] = ["00:00:05:00", "00:00:20:00"]
+timeline = timeline_json["timeline"]
+timecodes: List[str] = [t["time"] for t in timeline if config_id in t.keys()]
+
+print(timecodes)
+
 i: int = 0
 tc: Timecode = Timecode("30", frames=1)
 
@@ -38,6 +61,7 @@ while player.con:
 
     if len(timecodes) == i:
         continue
+
 
     if tc == timecodes[i]:
         if player.is_playing:
